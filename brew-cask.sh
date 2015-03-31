a8e76f9488e5c9c3bd5adc30f4011f131adc636e
Brad Clark <brad.clark@acquia.com>
2015-03-31 14:26:57 -0400

    update brew since Brewfile is deprecated, add brew cask

---
+++ brew-cask.sh
@@ -0,0 +1,24 @@
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# applications
+brew cask install adium
+brew cask install alfred
+brew cask install caffeine
+brew cask install dropbox
+brew cask install evernote
+brew cask install firefox
+brew cask install flux
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install iterm2
+brew cask install lastpass
+brew cask install menumeters
+brew cask install slack
+brew cask install spectacle
+brew cask install spotify
+brew cask install sublime-text3
+brew cask install tunnelblick
+brew cask install vagrant-manager
---
+++ brew.sh
@@ -0,0 +1,54 @@
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated)
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+# Install some other useful utilities like `sponge`
+brew install moreutils
+# Install GNU `sed`, overwriting the built-in `sed`
+brew install gnu-sed --default-names
+
+# Install Bash 4
+brew install bash
+
+# Install bash-completion
+brew tap homebrew/versions
+brew install homebrew/versions/bash-completion2
+brew install bash-completion
+brew tap homebrew/completions
+brew install vagrant-completion
+
+# generic colouriser  http://kassiopeia.juls.savba.sk/~garabik/software/grc/
+brew install grc
+
+# Install wget with IRI support
+brew install wget --enable-iri
+
+# Install more recent versions of some OS X tools
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/screen
+
+# Install everything else
+brew install ack
+brew install bfg
+brew install git
+brew install nmap
+brew install node
+brew install pv
+brew install rename
+brew install speedtest_cli
+brew install tree
+brew install terminal-notifier
+
+# Install some linters
+install shellcheck
+
+# Remove outdated versions from the cellar
+brew cleanup
