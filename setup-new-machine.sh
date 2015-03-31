c7c960ea9178bedf83a81fd288942e762ffc6c22
Brad Clark <brad.clark@acquia.com>
2015-03-31 14:28:27 -0400

    rename setup script

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -19,3 +19,9 @@ fi
 echo "Installing brew apps..."
 ./brew.sh
 ./brew-cask.sh
+
+# change to bash 4 (installed by homebrew)
+BASHPATH=$(brew --prefix)/bin/bash
+sudo echo $BASHPATH >> /etc/shells
+chsh -s $BASHPATH # will set for current user only.
+echo $BASH_VERSION # should be 4.x not the old 3.2.X
