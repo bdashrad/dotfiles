bf3c6680b61eb2766d496884e0e303fa587b2d55
Brad Clark <brad.clark@acquia.com>
2015-04-01 16:32:11 -0400

    added brew doctor execution

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -16,6 +16,7 @@ if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew doctor
 fi
 
 # install brew apps
