f4abcaa968552879423e8cec09cec6ee98f49056
Brad Clark <bdashrad@gmail.com>
2016-04-20 11:41:11 -0400

    easily delete merged branches - thanks paulirish

---
+++ bin/git-delete-merged-branches.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+branches_to_die=$(git branch --no-color --merged origin/master | grep -v '^[\* ] master')
+echo "Branches to be deleted:"
+echo $branches_to_die
+echo ""
+echo "Enter Y to confirm"
+read -p "> " confirm
+
+kill_branches(){
+  echo $branches_to_die | xargs -n 1 git branch -d
+}
+
+[[ $confirm == "Y"  ]] && kill_branches
