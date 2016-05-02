6f15610dd1950d6768598a68fef6fcfb92107d0e
Brad Clark <bdashrad@gmail.com>
2016-05-02 13:14:18 -0400

    add ssh control path support

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -35,3 +35,7 @@ BASHPATH=$(brew --prefix)/bin/bash
 echo $BASHPATH | sudo tee -a /etc/shells > /dev/null
 chsh -s $BASHPATH # will set for current user only.
 echo $BASH_VERSION # should be 4.x not the old 3.2.X
+
+# create controlpath folder for ssh
+mkdir -p ~/.ssh/control
+
