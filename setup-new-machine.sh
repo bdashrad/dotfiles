0dbb15ef6e8aa09d84386cfeaa1f4359d084e101
Brad Clark <bdashrad@gmail.com>
2016-08-11 12:02:56 -0400

    fix xcode install

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -6,12 +6,42 @@ sudo -v
 # update osx
 sudo softwareupdate -i -a
 
-# install xcode command line tools
-echo "Installing xcode-command line tools"
-xcode-select --install
+### XCode Command Line Tools
+#      thx https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
 
-# accept xcode license so command line tools work
-sudo xcodebuild -license
+if ! xcode-select --print-path &> /dev/null; then
+
+    # Prompt user to install the XCode Command Line Tools
+    xcode-select --install &> /dev/null
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait until the XCode Command Line Tools are installed
+    until xcode-select --print-path &> /dev/null; do
+        sleep 5
+    done
+
+    print_result $? 'Install XCode Command Line Tools'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Point the `xcode-select` developer directory to
+    # the appropriate directory from within `Xcode.app`
+    # https://github.com/alrra/dotfiles/issues/13
+
+    sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+    print_result $? 'Make "xcode-select" developer directory point to Xcode'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Prompt user to agree to the terms of the Xcode license
+    # https://github.com/alrra/dotfiles/issues/10
+
+    sudo xcodebuild -license
+    print_result $? 'Agree with the XCode Command Line Tools licence'
+
+fi
+###
 
 # install rvm stable
 echo "Installing rvm..."
