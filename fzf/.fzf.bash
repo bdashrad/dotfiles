312444aa2205c501f9be6c73239b8cf999fc76ab
Brad Clark <bdashrad@gmail.com>
2017-09-22 08:17:19 -0400

    update fzf

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -1,20 +1,14 @@
 # Setup fzf
 # ---------
-# if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-#   export PATH="$PATH:/usr/local/opt/fzf/bin"
-# fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
+if [[ ! "$PATH" == */Users/bradclark/.fzf/bin* ]]; then
+  export PATH="$PATH:/Users/bradclark/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/bradclark/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+source "/Users/bradclark/.fzf/shell/key-bindings.bash"
 
