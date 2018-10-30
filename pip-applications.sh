d88bbc8b55af2590a50efdd6615b9b97d531ad02
Brad Clark <bdashrad@gmail.com>
2018-10-30 16:41:46 -0400

    add some pip installed apps

--- pip-applications.sh
+++ pip-applications.sh
@@ -2,9 +2,18 @@
 
 # pip applications
 
+# upgrade pip
+pip install --upgrade pip
+pip3 install --upgrade pip
+
 # development
+pip install ansible-lint
+pip install botocore
+pip3 install boto
 pip install pygithub
+pip install requests
 pip install virtualenv
+pip3 install yamllint
 
 # fun
 pip install doge
