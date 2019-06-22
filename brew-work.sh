c8be94588b5836e62d533514f86c207676736ffb
Brad Clark <bdashrad@gmail.com>
2019-06-21 23:30:26 -0400

    fix flags on formula, separate work installs

---
+++ brew-work.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# work stuff
+
+# log parsing things
+# brew install lnav
+
+# infra management
+brew install ansible
+brew install terraform
+brew install vault
+
+# Install kubernetes stuff
+brew install kubernetes-cli
+brew install kubernetes-helm
+brew install kube-ps1
+brew install kubectx
+
+# Remove outdated versions from the cellar
+brew cleanup
--- brew.sh
+++ brew.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Make sure we’re using the latest Homebrew
-brew update
-
 # Upgrade any already-installed formulae
 brew upgrade
 
@@ -10,33 +7,44 @@ brew upgrade
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+# Don't forget to add `$(brew --prefix findutils)/libexec/gnubin` to `$PATH`.
 brew install findutils
 # Install some other useful utilities like `sponge`
 brew install moreutils
 # Install GNU `sed`, overwriting the built-in `sed`
-brew install gnu-sed --with-default-names
+# Don't forget to add `$(brew --prefix gnu-sed)/libexec/gnubin` to `$PATH`.
+brew install gnu-sed
+# Install GNU `grep`
+# Don't forget to add `$(brew --prefix grep)/libexec/gnubin` to `$PATH`.
+brew install grep
 
 # Install Bash 4
 brew install bash
 
+# Install/Upgrade some languages
+brew install go
+brew install node
+brew install python
+
+# stow is used to manage these dotfiles
+brew install stow
+
 # Install bash-completion
 brew install bash-completion
 brew install brew-cask-completion
-brew install vagrant-completion
 
 # generic colouriser  http://kassiopeia.juls.savba.sk/~garabik/software/grc/
 brew install grc
 
 # Install wget with IRI support
-brew install wget --with-iri
+brew install wget
 
 # Install r
-brew install r
-mkdir -p ~/Library/R/3.2/library
+# brew install r
+# mkdir -p ~/Library/R/3.2/library
 
 # Install more recent versions of some OS X tools
-brew install vim --with-override-system-vi
-brew install grep --default-names
+brew install vim
 brew install screen
 
 # run this script when this file changes guy.
@@ -54,28 +62,18 @@ brew install automake
 # brew install bfg
 brew install gist
 brew install git
-brew install go
-brew install heroku
-brew install hub
 brew install jq
-brew install node
 brew install openssl@1.1
-brew install python
+
+# install heroku-cli
+brew tap heroku/brew
+brew install heroku
 
 # network stuff
 brew install grepcidr
 brew install mtr
 brew install nmap
 brew install speedtest_cli
-
-# log parsing things
-brew install lnav
-brew install logstalgia
-
-# infra management
-brew install ansible
-brew install terraform
-brew install vault
 
 # Install everything else
 brew install aws-elasticbeanstalk
@@ -88,7 +86,6 @@ brew install imagemagick
 brew install ncdu
 brew install pv
 brew install rename
-brew install stow
 brew install swig
 brew install tree
 brew install terminal-notifier
@@ -105,12 +102,6 @@ brew install xz
 
 # Install some linters
 brew install shellcheck
-
-# Install kubernetes stuff
-brew install kubernetes-cli
-brew install kubernetes-helm
-brew install kube-ps1
-brew install kubectx
 
 # Remove outdated versions from the cellar
 brew cleanup
