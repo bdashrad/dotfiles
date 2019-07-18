03d610b6c6273d890b1bd6f8a8a1247c476f0f5e
Brad Clark <bdashrad@gmail.com>
2019-07-18 16:54:51 -0400

    manage some apps with asdf instead

--- brew-work.sh
+++ brew-work.sh
@@ -8,19 +8,27 @@
 brew install goaccess
 
 # infra management
-brew install ansible
+# brew install ansible
+# brew install ansible-lint
 brew install packer
-brew install terraform
+# brew install terraform
 brew install terraform-docs
 brew install terraform_landscape
 brew install vault
 
 # Install kubernetes stuff
-brew install kubernetes-cli
-brew install kubernetes-helm
-brew install kops
+# brew install kubernetes-cli
+# brew install kubernetes-helm
+# brew install kops
 brew install kube-ps1
 brew install kubectx
+
+# zoom
+pkill "ZoomOpener"; rm -rf ~/.zoomus; touch ~/.zoomus && chmod 000 ~/.zoomus;
+brew cask install zoomus
+defaults write ~/Library/Preferences/us.zoom.config.plist ZDisableVideo 1
+sudo defaults write /Library/Preferences/us.zoom.config.plist ZDisableVideo 1
+
 
 # Remove outdated versions from the cellar
 brew cleanup
