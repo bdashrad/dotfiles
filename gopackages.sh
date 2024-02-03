85dadb67817837a49b5767291d8d70ef9a2b7718
Brad Clark <bdashrad@gmail.com>
2024-02-03 15:23:56 -0500

    some go packages

---
+++ gopackages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# goweight
+# https://github.com/jondot/goweight
+go install github.com/jondot/goweight@latest
+
+# godotenv
+# https://github.com/joho/godotenv
+go install github.com/joho/godotenv/cmd/godotenv@latest
+
+# modgv
+# https://github.com/lucasepe/modgv
+go install github.com/lucasepe/modgv/modgv@latest
+
+# https://github.com/patcable/onair
+# go install github.com/patcable/onair
