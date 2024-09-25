8a9de53bd7c1fffed0346c25f4688652ccca2dca
Brad Clark <brad.clark@authorhealth.com>
2024-09-25 17:35:39 -0400

    update configs

--- blesh/.config/blesh/init.sh
+++ blesh/.config/blesh/init.sh
@@ -21,7 +21,7 @@ bleopt complete_auto_delay=500
 ## With the following settings, M-backspace (whose actual key sequence
 ## depends on your terminal) will kill the backward word as in the default
 ## readline
-ble-bind -f 'M-C-?' kill-backward-cword # macos
-# ble-bind -f 'M-DEL' kill-backward-cword
+ble-bind -f 'M-C-?' kill-backward-cword # escape-backspace, macos
+ble-bind -f 'M-DEL' kill-backward-cword # meta-backspace, macos
 # ble-bind -f 'M-C-h' kill-backward-cword
 # ble-bind -f 'M-BS'  kill-backward-cword
