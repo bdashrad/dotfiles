c53441222f12c05834d63642c7265793ca4a9798
Brad Clark <brad.clark@authorhealth.com>
2026-07-31 15:11:37 -0400

    setup XDG paths

--- setup/setup-new-machine.sh
+++ setup/setup-new-machine.sh
@@ -12,6 +12,9 @@ else
   exit 1
 fi
 
+# create XDG directories
+mkdir -p "${HOME}"/{.config,.cache,.local/{share,state}}
+
 # Ask for the administrator password upfront
 title "Ask for sudo access at start"
 sudo -v
